ec065de77e01cbf93ce6ce7a6da5951a6e087060
taanveerahmeed <taanveerahmeed@gmail.com>
2020-10-22 14:22:41 +0600

    PHP: Form Submission

---
+++ form_submission/Login.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+		<title>Login From</title>
+	</head>
+	<body>
+		<h1>Login Form</h1>
+		<form action="login_submitted.php" method="post">
+			<table >
+				<tr>
+					<td>Username:</td>
+					<td><input type="text" name="username" placeholder="Write your username"></td>
+				</tr>
+				<tr>
+					<td>Password:</td>
+					<td><input name="password" type="password"></td>
+				</tr>
+				<tr>
+					<td colspan="2" align="right">
+						<a href="Registration.html" target="_blank">Not registered yet?</a> &nbsp; &nbsp; &nbsp; &nbsp;
+						<input type="submit" value="Login">
+					</td>
+				</tr>
+			</table>
+			
+			  
+			
+		</form>
+	
+	</body>
+</html>
---
+++ form_submission/login_submitted.php
@@ -0,0 +1,10 @@
+<html>
+	<head></head>
+	<body>
+		<h1>Login Form Submitted</h1>
+		<?php
+			echo "Username : ". $_REQUEST["username"]."<br>" ;
+			echo "Password : ". $_REQUEST["password"]."<br>" ;
+		?>
+	</body>
+</html>
